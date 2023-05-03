1b62c959548e362b83ac6e20531800ce3532d611
supechicken <supechicken666@gmail.com>
2023-05-04 02:16:15 +0800

    Update debug message

--- preload.js
+++ preload.js
@@ -6,7 +6,10 @@ const wss = new WebSocketServer({ port: 25600 });
 let sendRequestToChrome, wsConnected = false;
 
 wss.on('connection', ws => {
-  ws.on('open', () => { wsConnected = true; });
+  ws.on('open', () => {
+    wsConnected = true;
+    console.log('extension connected!');
+  });
   ws.on('error', console.error);
   ws.on('message', data => console.log('received: %s', data));
 
