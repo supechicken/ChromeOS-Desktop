28fe5f5af46b2a73d53c701051253533a3707a2b
supechicken <supechicken666@gmail.com>
2023-05-04 02:02:27 +0800

    Auto reconnect to electron app

--- chrome_extension/ws_receiver/ws_receiver.js
+++ chrome_extension/ws_receiver/ws_receiver.js
@@ -1,12 +1,20 @@
 (async () => {
-  const ws = new WebSocket('ws://localhost:25600'),
-        sw = (await navigator.serviceWorker.ready).active;
+  const sw = (await navigator.serviceWorker.ready).active;
+  const connect_websocket = () => {
+    const ws = new WebSocket('ws://localhost:25600');
 
-  ws.onopen = () => console.log('[debug]', 'connection opened!');
-  ws.onmessage = e => {
-    const data = e.data;
+    ws.onopen    = () => console.log('[debug]', 'connection opened!');
+    ws.onclose   = () => {
+      console.log('[debug]', 'will reconnect after 1 seconds...')
+      setTimeout(connect_websocket, 1000);
+    };
 
-    console.log(data);
-    sw.postMessage(data);
-  }
+    ws.onmessage = e => {
+      const data = e.data;
+      console.log(data);
+      sw.postMessage(data);
+    }
+  };
+
+  connect_websocket();
 })();
